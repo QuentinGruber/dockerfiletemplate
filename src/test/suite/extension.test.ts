20cc343c1542cdb9e802fd424438ec92512d15cb
Quentin Gruber <quentingruber@gmail.com>
2021-02-16 21:19:15 +0100

    Update extension.test.ts

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,14 +9,14 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	test('CreateDockerFile', () => {
-		const templatePath = path.join(__dirname, '../templates');
+		const templatePath = path.join(__dirname, '../../../templates');
 		const { workspace: { workspaceFolders } } = vscode;
 		const workspaceFolder = workspaceFolders ? workspaceFolders[0].uri.fsPath : path.join(__dirname, '../');
 		assert.strictEqual(CreateDockerFile("Nodejs app",templatePath,workspaceFolder), true);
 	});
 
 	test('CreateIgnoreDockerFile', () => {
-		const templatePath = path.join(__dirname, '../templates');
+		const templatePath = path.join(__dirname, '../../../templates');
 		const { workspace: { workspaceFolders } } = vscode;
 		const workspaceFolder = workspaceFolders ? workspaceFolders[0].uri.fsPath : path.join(__dirname, '../');
 		assert.strictEqual(CreateIgnoreDockerFile("Nodejs app",templatePath,workspaceFolder), true);
