425e752f303495815027ee42d5fa081097ea34d0
Quentin Gruber <quentingruber@gmail.com>
2022-02-03 01:00:38 +0100

    deepscan

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { readFileSync, writeFileSync, readdirSync, PathLike } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { normalize } from "path";
 import { DockerFileTemplate } from "./views/dockerfiletemplate";
 
